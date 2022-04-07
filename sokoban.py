23f67fc443a44f522c68b4678675e4498fff2bb9
OSCAR ORTA <97772582+OscarOrta@users.noreply.github.com>
2022-04-07 12:49:26 +0000

    Valores corregidos

--- sokoban.py
+++ sokoban.py
@@ -2,10 +2,10 @@ print("Hola mundo")
 class Sokoban: #Se crea la clase llamada sokoban
   """
   Definimos los componentes
-  0-cajas
-  1-paredes
-  2-muñeco
-  3-caminos
+  0-personaje
+  1-espacio
+  2-caja
+  3-paredes
   4-metas
   5-muñeco_meta
   6-caja_meta
@@ -20,13 +20,13 @@ class Sokoban: #Se crea la clase llamada sokoban
 #t
   def cargarMapa(self): #cargar el mapa 
     self.mapa = [
-        [1,1,1,1,1,1,1,1,1], 
-        [1,3,3,1,1,1,1,1,1],
-        [1,3,3,3,3,0,3,3,1],
-        [1,3,1,1,3,3,2,3,1],
-        [1,4,1,1,0,1,0,3,1],
-        [1,4,1,1,3,3,3,3,1],
-        [1,1,1,1,1,1,1,1,1],
+        [3,3,3,3,3,3,3,3,3], 
+        [3,1,1,3,3,3,3,3,3],
+        [3,1,1,1,1,2,1,1,3],
+        [3,1,3,3,1,1,0,1,3],
+        [3,4,3,3,2,1,2,1,3],
+        [3,4,3,3,1,1,1,1,3],
+        [3,3,3,3,3,3,3,3,3],
     ]
 #v
   def imprimirMapa(self):#ta
@@ -35,32 +35,32 @@ class Sokoban: #Se crea la clase llamada sokoban
   def encontrarPosicionC(self):
       for fila in range(len(self.mapa)): # Get rows number on the map
         for columna in range(len(self.mapa[fila])): # Get columns number on the map
-          if self.mapa[fila][columna] == 2: # If the character is found
+          if self.mapa[fila][columna] == 0: # If the character is found
             self.posicion_fila = fila # Update the character row position
             self.posicion_columna = columna # Update the character col position
   def moverDerecha(self): #metodo para mover el personaje a la derecha
-    if self.mapa[self.posicion_fila][self.posicion_columna] == 2 and self.mapa[self.posicion_fila][self.posicion_columna + 1] == 3:
-      self.mapa[self.posicion_fila][self.posicion_columna] = 3 
-      self.mapa[self.posicion_fila][self.posicion_columna + 1] = 2
+    if self.mapa[self.posicion_fila][self.posicion_columna] == 0 and self.mapa[self.posicion_fila][self.posicion_columna + 1] == 1:
+      self.mapa[self.posicion_fila][self.posicion_columna] = 1
+      self.mapa[self.posicion_fila][self.posicion_columna + 1] = 0
       self.posicion_columna += 1
     
   def moverIzquierda(self): #metodo para mover el personaje a la izquierda
-    if self.mapa[self.posicion_fila][self.posicion_columna] == 2 and self.mapa[self.posicion_fila][self.posicion_columna - 1] == 3:
-      self.mapa[self.posicion_fila][self.posicion_columna] = 3 
-      self.mapa[self.posicion_fila][self.posicion_columna - 1] = 2
+    if self.mapa[self.posicion_fila][self.posicion_columna] == 0 and self.mapa[self.posicion_fila][self.posicion_columna - 1] == 1:
+      self.mapa[self.posicion_fila][self.posicion_columna] = 1 
+      self.mapa[self.posicion_fila][self.posicion_columna - 1] = 0
       self.posicion_columna = self.posicion_columna - 1 
 
   def moverArriba(self): #metodo para mover el personaje hacia arriba
-      if self.mapa[self.posicion_fila][self.posicion_columna] == 2 and self.mapa[self.posicion_fila - 1][self.posicion_columna] == 3: # If the character is on the floor and the next position is a floor
-          self.mapa[self.posicion_fila][self.posicion_columna] = 3 # put floor character last position
-          self.mapa[self.posicion_fila - 1][self.posicion_columna] = 2 # move the character to next position
+      if self.mapa[self.posicion_fila][self.posicion_columna] == 0 and self.mapa[self.posicion_fila - 1][self.posicion_columna] == 1: # If the character is on the floor and the next position is a floor
+          self.mapa[self.posicion_fila][self.posicion_columna] = 1 # put floor character last position
+          self.mapa[self.posicion_fila - 1][self.posicion_columna] = 0 # move the character to next position
           self.posicion_fila = self.posicion_fila - 1 # update the character position
 
 
   def moverAbajo(self): #metodo para mover el personaje hacia abajo
-      if self.mapa[self.posicion_fila][self.posicion_columna] == 2 and self.mapa[self.posicion_fila + 1][self.posicion_columna] == 3: # If the character is on the floor and the next position is a floor
-          self.mapa[self.posicion_fila][self.posicion_columna] = 3 # put floor character last position
-          self.mapa[self.posicion_fila + 1][self.posicion_columna] = 2 # move the character to next position
+      if self.mapa[self.posicion_fila][self.posicion_columna] == 0 and self.mapa[self.posicion_fila + 1][self.posicion_columna] == 1: # If the character is on the floor and the next position is a floor
+          self.mapa[self.posicion_fila][self.posicion_columna] = 1 # put floor character last position
+          self.mapa[self.posicion_fila + 1][self.posicion_columna] = 0 # move the character to next position
           self.posicion_fila = self.posicion_fila + 1 # update the character position 
 
     
